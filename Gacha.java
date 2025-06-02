15435356ab4fdee22796b31521436b027212b2df
daphnet9 <daphnet9@nycstudents.net>
2025-06-02 11:26:33 -0400

    Create Gacha.java

---
+++ Gacha.java
@@ -0,0 +1,30 @@
+//Daphne
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Gacha {
+    //arraylist with all the possible rolls
+    public static String[] pool = {
+        "(•‿•)", "(◑‿◐)", "…ᘛ⁐̤ᕐᐷ", "=＾● ⋏ ●＾="
+    };
+    
+    public static void setPool (ArrayList<String> gacha, String[] rolls) 
+    {
+        for(String i : rolls)
+        {
+            gacha.add(i);
+        }
+    }
+
+    public static String roll(ArrayList<String> pool) {
+        Random rand = new Random();
+        int index = rand.nextInt(pool.size());
+        String result = pool.get(index);
+        System.out.println("You rolled: " + result);
+        return result;
+    }
+
+    //method that returns the indexes of array (just a method for ourselves to keep track)
+
+    //method that randomizes a number in the range of the array and gets the item at that index (return function)
+}

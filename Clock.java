5203312899a05abafcc34c371d5a443ce36535ba
daphnet9 <daphnet9@nycstudents.net>
2025-06-02 11:27:08 -0400

    Create Clock.java

---
+++ Clock.java
@@ -0,0 +1,31 @@
+//Jia Yi
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Clock {
+    private int hours;
+    private int mins;
+    private int secs;
+
+    //update method? for the display but then how do you get the varaibles out
+    // public Clock (Task current)
+    // {
+    //     hours = current.getHours();
+    // }
+
+    public static void startTask() 
+    {
+        
+    //method startTimer
+    //contains local vars for minutes (starts at 59), and seconds(also starts at 59)
+    //uses timerTask method for every second , prints out the time thats updated
+    //all the timerTask method is doing is printing out the time
+    //actual code is within the start timer, which decrements the time within a nesed if statement
+    //and then also contains an integrated break when hour reaches a certain amount and if the starting hours was a lot//
+    //unscheduled break is through a timer
+        // TimerTask display = new TimerTask() {
+        //     public void run() {
+        //         System.out.println();
+        // }
+    };
+}

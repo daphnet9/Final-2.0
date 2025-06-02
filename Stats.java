153a68d11920d539d65e07893f69ea60fef9df20
daphnet9 <daphnet9@nycstudents.net>
2025-06-02 11:24:55 -0400

    Create Stats.java

---
+++ Stats.java
@@ -0,0 +1,27 @@
+//Daphne
+import java.util.ArrayList;
+
+public class Stats {
+    private int coins = 0;
+    private int exp = 0;
+    private String currentAvatar = "";
+    private ArrayList<String> avatars = new ArrayList<>();
+    private ArrayList<String> achievements = new ArrayList<>();
+    
+    public void addAvatar(String avatar) {
+        avatars.add(avatar);
+        currentAvatar = avatar;
+    }
+
+    public void 
+
+    //avatar array
+
+    //achievements array
+    
+    //vars for assignments completed, coin pouch, current avatar
+    //method for printing out whole achievement array (viewAchievements)
+    //method to update avatar AND another method to select avatar from arraylist
+
+    //add avatar to array(?) by making an addAvatar() which calls the random method from gacha and adds it//
+}
